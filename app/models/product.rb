ab63458ce2e10215005ac286d6773eb8f6010ff7
Phil <phil88530@gmail.com>
2012-01-08 17:02:02 +0000

    make validations for products and updated its testing case

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+	validates :title, :uniqueness => true
+
+	validates_presence_of :title, :description, :image_url, :price
+	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+	validates :image_url, :format => {
+		:with => %r{\.(gif|jpg|png)$}i,
+		:message => 'Image must be git,jpg or png format'
+	}
+
 end
