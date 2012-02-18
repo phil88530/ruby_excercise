e462d01e66d014efeb7d7c22cacba2a385c03736
Phil <phil88530@gmail.com>
2012-02-18 23:12:39 +0000

    make cart page able to sum up the prices with a clear option

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,7 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, :dependent => :destroy #depend on if line_item exist
 
+	#add a product to cart, if exist, add the quantity instead
 	def add_product(product_id)
 		current_item = line_items.find_by_product_id(product_id)
 		if current_item
@@ -9,5 +10,10 @@ class Cart < ActiveRecord::Base
 			current_item = line_items.build(:product_id => product_id)
 		end
 		current_item
+	end
+
+	#count total price of all items
+	def total_price
+		line_items.to_a.sum{|item| item.total_price}
 	end
 end
