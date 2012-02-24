2835583538e3c7418aa2194261001e9e4a0fe8a3
Phil <phil88530@gmail.com>
2012-02-24 19:07:52 +0000

    added highlight effect to new added items in cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -46,8 +46,9 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
+				@current_item = @line_item
         format.html { redirect_to store_url}
-				format.js{render :template => "carts/update_cart"}
+				format.js{ render :template => "carts/update_cart" }
 			else
         format.html { render :action => "new" }
         format.json { render :json => @line_item.errors, :status => :unprocessable_entity }
