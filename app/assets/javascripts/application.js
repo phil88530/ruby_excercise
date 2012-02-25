b6e2db40653078684c34dc6077ee454f6ef1bb90
Phil <phil88530@gmail.com>
2012-02-25 19:53:38 +0000

    fixing conflict issue with jquery and prototype by remove the prototype.ujs file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,9 +3,9 @@
 // be included in the compiled file accessible from http://example.com/assets/application.js
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
-//
+//=require jquery
+//=require jquery_ujs
 //= require prototype
-//= require prototype_ujs
 //= require effects
 //= require dragdrop
 //= require controls
