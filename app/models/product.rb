1bb141d8235d005612498763ea29f88fbae58cbd
YuFei Zhu <phil@mohc.net>
2012-05-10 16:47:05 +0100

    added view cart option inside a sidebar uinder layout

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ActiveRecord::Base
 
 	validates_presence_of :title, :description, :image_url, :price, :cover_image
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+
+  #only ned to use url if we trying to load an img from online
 	validates :image_url, :format => {
 		:with => %r{\.(gif|jpg|png)$}i,
 		:message => 'Image must be git,jpg or png format'
