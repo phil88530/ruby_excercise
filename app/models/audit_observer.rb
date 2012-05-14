15927b4a11c25105ded6b20c6bc42f1c05318a81
YuFei Zhu <phil@mohc.net>
2012-05-14 09:05:54 +0100

    added Product to the create observer list

--- app/models/audit_observer.rb
+++ app/models/audit_observer.rb
@@ -1,5 +1,5 @@
 class AuditObserver < ActiveRecord::Observer
-  observe Order, User
+  observe Order, User, Product
 
   def after_create(model)
     model.logger.info("[Audit] #{model.class.name} #{model.id} created")
