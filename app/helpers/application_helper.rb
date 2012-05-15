3884f2c5d7605eb512e6d4a309ea829a47eed1f5
YuFei Zhu <phil@mohc.net>
2012-05-15 10:58:38 +0100

    use helper to print weekdays on store index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,14 @@ module ApplicationHelper
 		content_tag("div", attributes, &block)
 	end
 
+  #print price is 1.3 times if language and price is set to spanish
   def t_currency(price)
     number_to_currency(price * (I18n.locale.to_s == 'es' ? 1.3 : 1))
+  end
+
+  #define 7 days of a week
+  DAYNAMES = %w{Sunday Monday Tuesday Wednesday Thursday Friday Saturday}
+  def print_weekday
+    DAYNAMES[Date.today.wday]
   end
 end
